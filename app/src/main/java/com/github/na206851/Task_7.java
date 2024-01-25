dbc0e1639c04802cdcc60b6301c7471e26e9f3d8
Nikita <na206851@gmail.com>
2024-01-25 13:26:23 +0800

    delete main method

--- app/src/main/java/com/github/na206851/Task_7.java
+++ app/src/main/java/com/github/na206851/Task_7.java
@@ -1,10 +1,6 @@
 package com.github.na206851;
 
 public class Task_7 {
-    public static void main(String[] args) {
-        Task_7 test = new Task_7();
-        test.arithmeticMethodSwapDigits1(1, 2);
-    }
 
     public int[] arithmeticMethodSwapDigits1(int n, int m) {
         n = n + m;
@@ -20,7 +16,7 @@ public class Task_7 {
         return new int[]{n, m};
     }
 
-    public int[] binMethod1SwapDigits(int n, int m) {
+    public int[] binMethodSwapDigits1(int n, int m) {
         n = n ^ m;
         m = n ^ m;
         n = m ^ n;

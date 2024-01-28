6b61a05f996bd481c690b41f0cf2a0194027297e
Nikita <na206851@gmail.com>
2024-01-28 16:10:33 +0800

    Add second method swap two digits through bit manipulation

--- app/src/main/java/com/github/na206851/Task_7.java
+++ app/src/main/java/com/github/na206851/Task_7.java
@@ -1,6 +1,12 @@
 package com.github.na206851;
 
+import java.util.Arrays;
+
 public class Task_7 {
+    public static void main(String[] args) {
+        Task_7 test = new Task_7();
+        System.out.println(Arrays.toString(test.binMethodSwapDigits2(Integer.parseInt("111", 2), Integer.parseInt("001", 2))));
+    }
 
     public int[] arithmeticMethodSwapDigits1(int n, int m) {
         n = n + m;
@@ -20,6 +26,13 @@ public class Task_7 {
         n = n ^ m;
         m = n ^ m;
         n = m ^ n;
+        return new int[]{n, m};
+    }
+
+    public int[] binMethodSwapDigits2(int n, int m) {
+        n = (n & m) + (n | m);
+        m = n + (~m) + 1;
+        n = n + (~m) + 1;
         return new int[]{n, m};
     }
 }

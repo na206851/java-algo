fd54c022ee3216686a4615dbf71883e782e476ba
Nikita <na206851@gmail.com>
2024-02-02 17:24:29 +0800

    Add explicit character encoding

---
+++ app/src/main/java/com/github/na206851/lesson2/Task_1.java
@@ -0,0 +1,11 @@
+package com.github.na206851.lesson2;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        System.out.println("Pėrshėndetje botė!");
+        System.out.println("\u0050\u0117\u0072\u0073\u0068\u0117\u006E\u0064\u0065\u0074\u006A\u0065\u0020" +
+                "\u0062\u006F\u0074\u0117\u0021");
+
+    }
+}
+

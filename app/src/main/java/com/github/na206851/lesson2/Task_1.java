64adc6f440888c47abae1da0f16d2751c9a508af
Nikita <na206851@gmail.com>
2024-02-04 01:55:23 +0800

    Add Task_1 encoding file

--- app/src/main/java/com/github/na206851/lesson2/Task_1.java
+++ app/src/main/java/com/github/na206851/lesson2/Task_1.java
@@ -5,7 +5,6 @@ public class Task_1 {
         System.out.println("Pėrshėndetje botė!");
         System.out.println("\u0050\u0117\u0072\u0073\u0068\u0117\u006E\u0064\u0065\u0074\u006A\u0065\u0020" +
                 "\u0062\u006F\u0074\u0117\u0021");
-
     }
 }
 

b7d289492497324c08fa710d5bd1f340511fc76b
Nikita <na206851@gmail.com>
2024-05-10 17:10:20 +0800

    удалил ненужные тесты, измерение работы в наносекундах

--- app/src/test/java/com/github/na206851/lesson3/SpeedTest.java
+++ app/src/test/java/com/github/na206851/lesson3/SpeedTest.java
@@ -4,95 +4,47 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.function.Consumer;
 
 public class SpeedTest {
-    @Test
-    void contatins() {
-        Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
-//            int element = (int) (Math.random() * 99999) + 1;
-            int endElement = 99999;
-            int middle = 50000;
-            int first = 1;
-            for (int i = 0; i < 100000; i++) {
-                list.add(i);
-            }
-            long start = System.currentTimeMillis();
-            list.contains(endElement);
-            long end = System.currentTimeMillis();
-            long result = end - start;
-            System.out.println(endElement + " element, " + result + " result speed contains");
-
-        };
-        listConsumer.accept(new ArrayList<>());
-        listConsumer.accept(new DynamicArray<>());
-    }
 
     @Test
-    void toArray() {
+    void addOneIteration() {
         Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
-            for (int i = 0; i < 100000; i++) {
-                list.add(i);
-            }
-            long start = System.currentTimeMillis();
-            list.toArray();
-            long end = System.currentTimeMillis();
+
+            long start = System.nanoTime();
+            list.add(1);
+            long end = System.nanoTime();
 
             long result = end - start;
-            System.out.println(result + " toArray speed");
+            System.out.println(result + " add one operation");
         };
-        listConsumer.accept(new ArrayList<>());
+        //listConsumer.accept(new ArrayList<>());
         listConsumer.accept(new DynamicArray<>());
     }
 
     @Test
-    void toArrayCollection() {
+    void addMillionElement() {
         Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
-            for (int i = 0; i < 100000; i++) {
+
+            long start = System.nanoTime();
+            for (int i = 0; i < 1_000_000; i++) {
                 list.add(i);
             }
-
-            Integer[] in = new Integer[list.size() + 1];
-
-            long start = System.currentTimeMillis();
-            list.toArray(in);
-            long end = System.currentTimeMillis();
+            long end = System.nanoTime();
 
             long result = end - start;
-            System.out.println(result + "toArrayCollection speed");
+            System.out.println(result + " add one million element");
         };
-        listConsumer.accept(new ArrayList<>());
+        //listConsumer.accept(new ArrayList<>());
         listConsumer.accept(new DynamicArray<>());
     }
 
     @Test
-    void add() {
-        Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
-            int count = 0;
-            long result = 0;
-            while (count < 3) {
-                long start = System.currentTimeMillis();
-
-                for (int i = 0; i < 100000; i++) {
-                    list.add(i);
-                }
-                long end = System.currentTimeMillis();
-
-                result += (end - start) / 3;
-                count++;
-            }
-            System.out.println(result + " add test speed");
-        };
-        listConsumer.accept(new ArrayList<>());
-        listConsumer.accept(new DynamicArray<>());
-    }
-
-    @Test
-    void addAddNotIndex() {
+    void addAllInTheEnd() { //конец списка
         Consumer<List<Integer>> listConsumer = (List<Integer> list) -> { //измерение в нано секундах
             ArrayList<Integer> in = new ArrayList<>();
-            for (int i = 0; i < 100000; i++) {
+            for (int i = 0; i < 1_000_000; i++) {
                 list.add(i);
             }
             for (int j = 0; j < 1000; j++) {
@@ -111,22 +63,44 @@ public class SpeedTest {
     }
 
     @Test
-    void addAllIndex() {
+    void addAllBegginIndex() { //начало списка
         Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
-            int index = (int) (Math.random() * 1000) + 1;
             ArrayList<Integer> in = new ArrayList<>();
-
-            for (int i = 0, j = 0; i < 100000 || j < 1000; i++, j++) {
-                in.add(j);
+            for (int i = 0; i < 1_000_000; i++) {
                 list.add(i);
             }
+            for (int j = 0; j < 1000; j++) {
+                in.add(j);
+            }
 
-            long start = System.currentTimeMillis();
-            list.addAll(index, in);
-            long end = System.currentTimeMillis();
+            long start = System.nanoTime();
+            list.addAll(0, in);
+            long end = System.nanoTime();
 
             long result = end - start;
-            System.out.println(index + " index in, " + result + " time addAll(index)");
+            System.out.println(result + " speed addAll no index");
+        };
+        listConsumer.accept(new ArrayList<>());
+        listConsumer.accept(new DynamicArray<>());
+    }
+
+    @Test
+    void addAllMiddleIndex() { //середина списка
+        Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
+            ArrayList<Integer> in = new ArrayList<>();
+            for (int i = 0; i < 1_000_000; i++) {
+                list.add(i);
+            }
+            for (int j = 0; j < 1000; j++) {
+                in.add(j);
+            }
+
+            long start = System.nanoTime();
+            list.addAll(500_000, in);
+            long end = System.nanoTime();
+
+            long result = end - start;
+            System.out.println(result + " speed addAll no index");
         };
         listConsumer.accept(new ArrayList<>());
         listConsumer.accept(new DynamicArray<>());
@@ -135,33 +109,65 @@ public class SpeedTest {
     @Test
     void addIndex() {
         Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
-            int index = (int) (Math.random() * 99999) + 1;
-            int in = -1;
-            for (int i = 0; i < 100000; i++) {
-                list.add(i);
-            }
 
-            long start = System.currentTimeMillis();
-            list.add(index, in);
-            long end = System.currentTimeMillis();
+            long start = System.nanoTime();
+            for (int i = 0; i < 10_000; i++) {
+                list.add(0, i);
+            }
+            long end = System.nanoTime();
 
             long result = end - start;
-            System.out.println(result + " time add(int index)" + index + " index");
+            System.out.println(result + " time add(int index)");
         };
-        listConsumer.accept(new ArrayList<>());
+        //listConsumer.accept(new ArrayList<>());
         listConsumer.accept(new DynamicArray<>());
     }
 
+    @Test
+    void addIndexOneIteration() {
+        Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
+            for (int i = 0; i < 1_000_000; i++) {
+                list.add(i);
+            }
+
+            long start = System.nanoTime();
+            list.add(500_000, -1);
+            long end = System.nanoTime();
+
+            long result = end - start;
+            System.out.println(result + " time add(int index)");
+        };
+        listConsumer.accept(new ArrayList<>());
+        // listConsumer.accept(new DynamicArray<>());
+    }
 
     @Test
-    void remove() {
+    void removeOneIteration() {
         Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
-            for (int i = 0; i < 100000; i++) {
+            for (int i = 0; i < 1_000_000; i++) {
                 list.add(i);
             }
 
             long start = System.currentTimeMillis();
-            for (int i = 99999; i >= 0; i--) {
+            list.remove(500_000);
+            long end = System.currentTimeMillis();
+
+            long result = end - start;
+            System.out.println(result + " remove speed");
+        };
+        // listConsumer.accept(new ArrayList<>());
+        listConsumer.accept(new DynamicArray<>());
+    }
+
+    @Test
+    void removeMillionIteration() {
+        Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
+            for (int i = 0; i < 1_000_000; i++) {
+                list.add(i);
+            }
+
+            long start = System.currentTimeMillis();
+            for (int i = 999_999; i >= 0; i--) {
                 list.remove(i);
             }
             long end = System.currentTimeMillis();
@@ -169,157 +175,7 @@ public class SpeedTest {
             long result = end - start;
             System.out.println(result + " remove speed");
         };
-        listConsumer.accept(new ArrayList<>());
-        listConsumer.accept(new DynamicArray<>());
-    }
-
-    @Test
-    void removeObject() {
-        Consumer<List<String>> listConsumer = (List<String> list) -> {
-            for (int i = 0; i < 100000; i++) {
-                list.add("test");
-            }
-
-            long start = System.currentTimeMillis();
-            for (int i = 99999; i >= 0; i--) {
-                list.remove("test");
-            }
-            long end = System.currentTimeMillis();
-            long result = end - start;
-
-            System.out.println(result + " removeObject speed");
-        };
-        listConsumer.accept(new ArrayList<>());
-        listConsumer.accept(new DynamicArray<>());
-    }
-
-    @Test
-    void removeAll() {
-        Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
-            ArrayList<Integer> in = new ArrayList<>();
-
-            for (int i = 0, j = 0; i < 100000 || j < 100000; i++, j += 2) {
-                list.add(i);
-                in.add(j);
-            }
-
-            long start = System.currentTimeMillis();
-            list.removeAll(in);
-            long end = System.currentTimeMillis();
-
-            long result = end - start;
-            System.out.println(result + " removeAll speed");
-        };
-        listConsumer.accept(new ArrayList<>());
-        listConsumer.accept(new DynamicArray<>());
-    }
-
-    @Test
-    void retainAll() {
-        Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
-            List<Integer> in = new ArrayList<>();
-            for (int i = 0, j = 0; i < 100000 || j < 100000; i++, j += 3) {
-                list.add(i);
-                in.add(j);
-            }
-            long start = System.currentTimeMillis();
-            list.retainAll(in);
-            long end = System.currentTimeMillis();
-
-            long result = end - start;
-            System.out.println(result + " retainAll speed");
-        };
-        listConsumer.accept(new ArrayList<>());
-        listConsumer.accept(new DynamicArray<>());
-    }
-
-    @Test
-    void clear() {
-        Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
-            for (int i = 0; i < 100000; i++) {
-                list.add(i);
-            }
-            long start = System.currentTimeMillis();
-            list.clear();
-            long end = System.currentTimeMillis();
-
-            long result = end - start;
-            System.out.println(result + " speed clear");
-        };
-        listConsumer.accept(new ArrayList<>());
-        listConsumer.accept(new DynamicArray<>());
-    }
-
-    @Test
-    void get() {
-        Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
-//            int elementGet = (int) (Math.random() * 99999) + 1;
-            int elementGet=50000;
-            for (int i = 0; i < 100000; i++) {
-                list.add(i);
-            }
-            long start = System.currentTimeMillis();
-            list.get(elementGet);
-            long end = System.currentTimeMillis();
-
-            long result = end - start;
-            System.out.println(result + " result get");
-        };
-        listConsumer.accept(new ArrayList<>());
-        listConsumer.accept(new DynamicArray<>());
-    }
-
-    @Test
-    void set() {
-        Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
-            int firstElement = 0;
-            int middleElement = 50000;
-            int endElement = 99999;
-            for (int i = 0; i < 100000; i++) {
-                list.add(i);
-            }
-            long start = System.currentTimeMillis();
-            list.set(endElement, -1);
-            long end = System.currentTimeMillis();
-
-            long result = end - start;
-            System.out.println(result + " speed set ");
-        };
-        listConsumer.accept(new ArrayList<>());
-        listConsumer.accept(new DynamicArray<>());
-    }
-
-    @Test
-    void indexOf() {
-        Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
-            for (int i = 0; i < 100000; i++) {
-                list.add(i);
-            }
-            long start = System.currentTimeMillis();
-            list.indexOf(50000);
-            long end = System.currentTimeMillis();
-
-            long result = end - start;
-            System.out.println(result + " indexOf speed");
-        };
-        listConsumer.accept(new ArrayList<>());
-        listConsumer.accept(new DynamicArray<>());
-    }
-
-    @Test
-    void lastIndexOf() {
-        Consumer<List<Integer>> listConsumer = (List<Integer> list) -> {
-            for (int i = 0; i < 100000; i++) {
-                list.add(i);
-            }
-            long start = System.nanoTime();
-            list.lastIndexOf(50000);
-            long end = System.nanoTime();
-
-            long result = end - start;
-            System.out.println(result + " lastIndexOf speed");
-        };
-        listConsumer.accept(new ArrayList<>());
+        // listConsumer.accept(new ArrayList<>());
         listConsumer.accept(new DynamicArray<>());
     }
 }

7d703632e6329d49f1356e27c420d71048f1d543
Nikita Shavrov <na206851@gmail.com>
2024-08-30 00:45:02 +0800

    класс стек который написал

--- app/src/main/java/com/github/na206851/lesson5/MyStack.java
+++ app/src/main/java/com/github/na206851/lesson5/MyStack.java
@@ -2,16 +2,6 @@ package com.github.na206851.lesson5;
 
 import com.github.na206851.lesson4.DoublyLinkedList;
 
-import java.util.LinkedList;
+public class MyStack<E> extends DoublyLinkedList<E> {
 
-class MyStack<E> {
-    private DoublyLinkedList<E> list;
-
-    void push(E element) {
-        list.add(element);
-    }
-
-    E pop() {
-        return (E) list.remove();
-    }
 }

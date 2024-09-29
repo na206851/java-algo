1efad25285cc2528b4ce6b17b1f49926da0bf21c
Nikita Shavrov <na206851@gmail.com>
2024-09-29 11:28:11 +0800

    rename method and type returned value in method get

--- app/src/main/java/com/github/na206851/lesson6/Map.java
+++ app/src/main/java/com/github/na206851/lesson6/Map.java
@@ -1,9 +1,9 @@
 package com.github.na206851.lesson6;
 
 public interface Map<K, V> {
-    void add(K key, V Value);
+    void put(K key, V Value);
 
-    Node remove(K key);
+    V remove(K key);
 
-    V search(K key);
+    V get(K key);
 }

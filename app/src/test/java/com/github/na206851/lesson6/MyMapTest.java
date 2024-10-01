b2c999ee6ec54cb4e4b35e21f1975ae354b80a47
Nikita Shavrov <na206851@gmail.com>
2024-10-01 14:31:43 +0800

    добавил тесты на метода get, put, remove, resize

---
+++ app/src/test/java/com/github/na206851/lesson6/MyMapTest.java
@@ -0,0 +1,62 @@
+package com.github.na206851.lesson6;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MyMapTest {
+    @Test
+    void resizeTest() {
+        MyMap<Integer, Integer> myMap = new MyMap<>();
+        HashMap<Integer, Integer> jdkMap = new HashMap<>();
+        for (int i = 0; i < 1000; i++) {
+            myMap.put(i, i);
+            jdkMap.put(i, i);
+        }
+        assertEquals(jdkMap.toString(), myMap.toString());
+        assertEquals(jdkMap.size(), myMap.size());
+    }
+
+    @Test
+    void put() {
+        MyMap<String, Integer> myMap = new MyMap<>();
+        HashMap<String, Integer> jdkMap = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            myMap.put(String.valueOf(i), i);
+            jdkMap.put(String.valueOf(i), i);
+        }
+        assertEquals(jdkMap.toString(), myMap.toString());
+        assertEquals(jdkMap.get("3"), myMap.get("3"));
+    }
+
+    @Test
+    void remove() {
+        MyMap<String, Integer> myMap = new MyMap<>();
+        HashMap<String, Integer> jdkMap = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            myMap.put(String.valueOf(i), i);
+            jdkMap.put(String.valueOf(i), i);
+        }
+        assertEquals(1, myMap.remove("1"));
+        assertEquals(1, jdkMap.remove("1"));
+        assertEquals(jdkMap.toString(), myMap.toString());
+        assertEquals(2, myMap.remove("2"));
+        assertEquals(2, jdkMap.remove("2"));
+    }
+
+    @Test
+    void get() {
+        MyMap<String, Integer> myMap = new MyMap<>();
+        HashMap<String, Integer> jdkMap = new HashMap<>();
+        for (int i = 0; i < 10000; i++) {
+            myMap.put(String.valueOf(i), i);
+            jdkMap.put(String.valueOf(i), i);
+        }
+        assertEquals(jdkMap.get("7"), myMap.get("7"));
+        assertEquals(jdkMap.get("9999"), myMap.get("9999"));
+        assertEquals(jdkMap.get("-1"), myMap.get("-1"));
+        assertEquals(null, myMap.get("-12"));
+    }
+}

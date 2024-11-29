54cd8bdabfb80a346480a0d9aa14d41bf6af3adf
Nikita Shavrov <na206851@gmail.com>
2024-11-29 14:06:43 +0800

    добавлен метод добавления корня бинарного дерева

--- app/src/main/java/com/github/na206851/lesson7/tree/binary/LinkedBinaryTree.java
+++ app/src/main/java/com/github/na206851/lesson7/tree/binary/LinkedBinaryTree.java
@@ -31,7 +31,8 @@ public class LinkedBinaryTree<E> extends AbstractBinaryTree<E> {
 
     @Override
     public Node<E> addRoot(E e) throws IllegalStateException {
-        root = new LinkedBinaryTree.NodeImpl<>(e);
+        root = new NodeImpl<>(e);
+        size++;
         return root;
     }
 

138b92ad9ba2fa16247c58b2087ffb16881180cf
Nikita Shavrov <na206851@gmail.com>
2024-12-09 13:07:26 +0800

    добавили метод который приводит тип Node к нужному NodeImpl

--- app/src/main/java/com/github/na206851/lesson7/tree/binary/LinkedBinaryTree.java
+++ app/src/main/java/com/github/na206851/lesson7/tree/binary/LinkedBinaryTree.java
@@ -23,7 +23,7 @@ public class LinkedBinaryTree<E> extends AbstractBinaryTree<E> {
      * @throws IllegalArgumentException
      */
     protected NodeImpl<E> validate(Node<E> n) throws IllegalArgumentException {
-        return null;
+        return (NodeImpl<E>) n;
     }
 
     // update methods supported by this class

7f759154c21bffc64a55c04b5f45c9e17cb4a9e1
Nikita Shavrov <na206851@gmail.com>
2024-12-17 14:22:31 +0800

    добавил основу класса для бинарного дерева поиска

---
+++ app/src/main/java/com/github/na206851/lesson7/tree/binary/BinarySearchTree.java
@@ -0,0 +1,122 @@
+package com.github.na206851.lesson7.tree.binary;
+
+import com.github.na206851.lesson7.tree.Node;
+
+import java.util.Iterator;
+
+/**
+ * Concrete implementation of a binary tree using a node-based, linked structure
+ *
+ * @param <E> element
+ */
+public class BinarySearchTree<E> extends AbstractBinaryTree<E> {
+    Node<E> root;
+    int size = 0;
+    // nonpublic utility
+
+    /**
+     * Validates the node is an instance of supported {@link NodeImpl} type and casts to it
+     *
+     * @param n node
+     * @return casted {@link NodeImpl} node
+     * @throws IllegalArgumentException
+     */
+    protected NodeImpl<E> validate(Node<E> n) throws IllegalArgumentException {
+        return null;
+    }
+
+    // update methods supported by this class
+
+    @Override
+    public Node<E> addRoot(E e) throws IllegalStateException {
+        return null;
+    }
+
+    @Override
+    public Node<E> add(Node<E> n, E e) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public Node<E> addLeft(Node<E> n, E e) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public Node<E> addRight(Node<E> n, E e) throws IllegalArgumentException {
+        return null;
+    }
+
+    /**
+     * Replaces the element at {@link Node} <i>n</i> with <i>e</i>
+     *
+     * @param n node
+     * @param e element
+     * @return replace element
+     * @throws IllegalArgumentException
+     */
+    @Override
+    public E set(Node<E> n, E e) throws IllegalArgumentException {
+        return null;
+    }
+
+    /**
+     * Replaces the element at {@link Node} <i>n</i> with <i>e</i>
+     *
+     * @param n node
+     * @return replace element
+     * @throws IllegalArgumentException
+     */
+    @Override
+    public E remove(Node<E> n) throws IllegalArgumentException {
+        return null;
+    }
+
+    // {@link Tree} and {@link BinaryTree} implementations
+
+    @Override
+    public Node<E> left(Node<E> p) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public Node<E> right(Node<E> p) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public Node<E> root() {
+        return null;
+    }
+
+    @Override
+    public Node<E> parent(Node<E> n) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Iterable<Node<E>> nodes() {
+        return null;
+    }
+
+
+    protected static class NodeImpl<E> implements Node<E> {
+
+        @Override
+        public E getElement() {
+            return null;
+        }
+    }
+
+}
+

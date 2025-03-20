045f9d31f5f70e766001e38007bb9bae0452bfc5
Nikita Shavrov <na206851@gmail.com>
2025-03-21 01:00:18 +0800

    каркас для тестов sorted set

---
+++ app/src/test/java/com/github/na206851/lesson9/SortedSetTest.java
@@ -0,0 +1,16 @@
+package com.github.na206851.lesson9;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortedSetTest {
+
+
+}

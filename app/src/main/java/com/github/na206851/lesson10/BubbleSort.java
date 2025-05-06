d56eed1620ee261cd9d1be3385de566f3c35eac5
Nikita Shavrov <na206851@gmail.com>
2025-05-06 13:06:54 +0800

    изменил реализацию метода для работы с обобщенным типом

--- app/src/main/java/com/github/na206851/lesson10/BubbleSort.java
+++ app/src/main/java/com/github/na206851/lesson10/BubbleSort.java
@@ -7,12 +7,12 @@ public class BubbleSort<E extends Comparable<E>> implements Sort<E> {
         return bubbleSort(arr);
     }
 
-    public static int[] bubbleSort(int[] arr) {
+    public E[] bubbleSort(E[] arr) {
         boolean replacement = true;
         while (replacement != false) {
             replacement = false;
             for (int i = 1; i < arr.length; i++) {
-                if (arr[i - 1] > arr[i]) {
+                if (arr[i - 1].compareTo(arr[i]) > 0) {
                     swap(arr, i);
                     replacement = true;
                 }
@@ -21,10 +21,11 @@ public class BubbleSort<E extends Comparable<E>> implements Sort<E> {
         return arr;
     }
 
-    public static void swap(int[] arr, int i) {
-        int tmp = arr[i - 1];
+    public void swap(E[] arr, int i) {
+        E tmp = arr[i - 1];
         arr[i - 1] = arr[i];
         arr[i] = tmp;
+    }
 
     }
 }

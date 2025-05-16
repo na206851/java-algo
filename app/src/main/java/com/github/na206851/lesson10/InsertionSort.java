037f45f2bcfbf4b47840f2623cbd587c7de7a59d
Nikita Shavrov <na206851@gmail.com>
2025-05-17 02:58:17 +0800

    изменил метод сортировки вставкой

--- app/src/main/java/com/github/na206851/lesson10/InsertionSort.java
+++ app/src/main/java/com/github/na206851/lesson10/InsertionSort.java
@@ -1,13 +1,18 @@
 package com.github.na206851.lesson10;
 
-public class InsertionSort {
+public class InsertionSort<E extends Comparable<E>> implements Sort<E> {
 
-    public int[] insertionSort(int[] arr) {
+    @Override
+    public E[] sort(E[] arr) {
+        return insert(arr);
+    }
+
+    private E[] insert(E[] arr) {
         for (int i = 1; i < arr.length; i++) {
-            int tmp = arr[i];
-            int j = i - 1;
-            while (j >= 0 && arr[j] > tmp) {
-                j--;
+            E tmp = arr[i];
+            int j;
+            for (j = i - 1; j >= 0 && arr[j].compareTo(tmp) > 0; j--) {
+                arr[j + 1] = arr[j];
             }
             arr[j + 1] = tmp;
         }
